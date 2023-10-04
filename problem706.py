bef9132c96f8e18d71661da1e67b035804e1b87d
Chris Andersen <kraken027@yahoo.com>
2023-10-04 12:14:06 -0400

    problem 706 for 10/4/2023 initial problem

---
+++ problem706.py
@@ -0,0 +1,32 @@
+"""
+Problem 706. Design HashMap
+
+Design a HashMap without using any built-in hash table libraries.
+
+Implement the MyHashMap class:
+
+ - MyHashMap() initalizes the object with an empty map.
+
+ - void put(int key, int value) inserts a (key, value) pair into the HashMap. If the key already exists in the map, update the corresponding value.
+
+ - int get(int key) returns the value to which the specified key is mapped, or -1 if the map contains no mapping for the key.
+
+ - void remove(key) removes the key and its corresponding value if the map contains the mapping for the key.
+
+
+
+"""
+
+class MyHashMap:
+
+    def __init__(self) -> None:
+        pass
+
+    def put(self, key, value):
+        pass
+
+    def get(self, key):
+        pass 
+
+    def remove(self, key):
+        pass
