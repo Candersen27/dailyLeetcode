ed18c755e4e76991b8363391e3c333096a4b3b29
Chris Andersen <kraken027@yahoo.com>
2023-10-11 13:18:11 -0400

    probelm 2251 (hard) 10/11/2023 0.2 finished code and comments

--- problem2251.py
+++ problem2251.py
@@ -29,15 +29,15 @@ Strategy:
 
 This problem is made difficult by how large the constraints are.  The size of start_i and end_i as well as people_i can be up to a billion.  This rules out using something like a bucket_sort algorithm because the range of the sorting array will be too large.  When n is a billion, even O(n) wont work.  Similarly, an array size for people and flowers of fifty-thousand will make an algorithm with O(n^2) not viable as well.
 
-Instead we can use a tree to store flowers that are in bloom, and iterate through both flowers and people concurrently.  This will allow us to keep O(n(log(n))).
+Instead we can use a min heap to store flowers that are in bloom, and iterate through both flowers and people concurrently.  This will allow us to keep O(n(log(n))). A min heap is useful for this problem because we will constantly be removing the min value.
 
 First we want to sort flowers by bloom time (first element), and sort people.
 
-Next we want to build a tree to put the flowers which are in bloom, and change flowers to a deque so we can pop elements from the left.
+Next we want to build a heap to put the flowers which are in bloom, and change flowers to a deque so we can pop elements from the left.
 
 We want to iterate through the people array.
     For each person, we pop elements from the flowers deque where the bloom time is <= the visit time, until we hit a value that is larger.
-    We take the wither time (second element) and insert them into our tree.
+    We take the wither time (second element) and insert them into our heap.
     Remove from the tree all elements with a value < the visit value.
     Count the total number of elements in the tree, and return that value to answer[i]
 
@@ -45,7 +45,62 @@ We want to iterate through the people array.
 
 """
 
+import heapq
+from collections import deque
 
 class Solution:
     def fullBloomFlowers(self, flowers, people):
-        pass
+        
+        # First sort our flowers array and people array
+
+        # This will sort our flowers array by the value in the first element of each tuple
+        flowers.sort(key = lambda x: x[0])
+
+        # Our return values need to be associated with the original order of our people array, this creates a list for people that will be sorted based on their values, but will also hold the value of their original index.
+        people = sorted((p, i) for i, p  in enumerate(people))
+
+        # Turn flowers into a deque
+        flowers = deque(flowers)
+
+        # Initialize an empty heap
+        flower_heap = []
+
+        # Initialize an empty answer array
+        answer = []
+
+
+        for arrival, index in people:
+
+            # Add flowers to the heap which have boomed as or before the person gets there
+            # We plan on popping flowers[0] upon success, creating a new flowers[0], so always check element 0
+            while flowers and flowers[0][0] <= arrival:
+               
+                # Assign the first element of flowers[0] to bloom, and the second to wither
+                bloom, wither = flowers.popleft()
+                # Add the wither value of the flower to our heap
+                heapq.heappush(flower_heap, wither)
+                
+
+            # Remove flowers from the heap which have since withered. The flower with the lowest wither value will always be at the top in a min heap
+            
+            while flower_heap and flower_heap[0] < arrival:
+                heapq.heappop(flower_heap)
+
+            # The number of flowers still on the heap will be the total number in bloom for any given person           
+            answer.append((index, len(flower_heap)))
+        
+        # Sort the answer by the original indices
+        answer.sort()
+
+        return [count for _, count in answer]
+
+
+Sol = Solution
+
+F1, P1 = [[1,6],[3,7],[9,12],[4,13]], [2,3,7,11]
+F2, P2 = [[1,10],[3,3]], [3,3,2]
+
+
+
+print(Sol.fullBloomFlowers(Sol, F1, P1))
+print(Sol.fullBloomFlowers(Sol, F2, P2))
